34105f3beea7e54d8530073a70fae2b747bc42c6
hassen <hassen.rahali@esprit.tn>
2023-03-09 18:08:28 +0100

    product update

--- back-end/routes/product.js
+++ back-end/routes/product.js
@@ -11,7 +11,7 @@ router.post('/add', auth.authenticateToken, checkRole.checkRole, (req, res) => {
     var query = "insert into product (name, categoryId, description, price, status) values(?,?,?,?, 'true')";
     connection.query(query, [product.name, product.categoryId, product.description, product.price], (err, results) => {
         if (!err) {
-            return res.status(200).jsen({ message: "Product Added Successfully." });
+            return res.status(200).json({ message: "Product Added Successfully." });
         }
         else {
             return res.status(500).json(err);
@@ -30,5 +30,81 @@ router.get('/get', auth.authenticateToken, (req, res, next) => {
      }
     })
     })
+
+router.get('/getByCategory/:id', auth.authenticateToken, (req, res, next) =>{
+    const id = req.params.id
+    var query = " select id,name from product where categoryId =? and status = 'true'";
+    connection.query(query,[id], (err,results) =>{
+        if (!err){
+            return res.status(200).json(results);
+        }
+        else {
+            return res.status(500).json(err);
+        }
+    })
+})
+
+router.get('/getById/:id', auth.authenticateToken, (req, res, next) =>{
+    const id = req.params.id
+    var query = " select id,name,description,price from product where id =? "
+    connection.query(query,[id], (err,results) =>{
+        if (!err){
+            return res.status(200).json(results[0]);
+        }
+        else {
+            return res.status(500).json(err);
+        }
+    })
+})
+
+router.put('/update',auth.authenticateToken, checkRole.checkRole, (req, res) =>{
+    let product = req.body;
+    var query ="update product set name=?, categoryId=?, description=?, price=? where id=?"
+    connection.query(query,[product.name,product.categoryId,product.description,product.price,product.id],(err,results)=>{
+    if (!err){
+        if(results.affectedRows == 0){
+            return res.status(404).json({message:"Product ID does no found"});
+        }
+        return res.status(200).json({message:"Product Updated Successfully"});
+    }
+    else {
+        return res.status(500).json(err);
+    }
+})
+})
+
+router.delete('/delete/:id',auth.authenticateToken, checkRole.checkRole, (req, res) =>{
+    const id = req.params.id
+    var query ="delete from product where id=?"
+    connection.query(query,[id], (err,results) =>{
+        if (!err){
+            if(results.affectedRows == 0){
+                return res.status(404).json({message:"Product ID does no found"});
+            }
+            return res.status(200).json({message:"Product Deleted Successfully"});
+        }
+        else {
+            return res.status(500).json(err);
+            console.log(err)
+        }
+    })
+})
+router.put('/updateStatus',auth.authenticateToken, checkRole.checkRole, (req, res) =>{
+    let user = req.body;
+    var query ="update product set status=? where id=?"
+    connection.query(query,[user.status,user.id],(err,results)=>{
+        if (!err){
+            if(results.affectedRows == 0){
+                return res.status(404).json({message:"Product ID does no found"});
+            }
+            return res.status(200).json({message:"Product status Updated Successfully"});
+        }
+        else {
+            return res.status(500).json(err);
+        }
+    })
+})
+
+
 
     module.exports = router ;
