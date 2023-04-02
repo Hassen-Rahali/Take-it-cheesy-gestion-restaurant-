8b1f40275d8954e8f441082046312666d8c5be55
Hassen-Rahali <hassen.rahali@esprit.tn>
2023-04-02 03:51:57 +0200

    create sign up

---
+++ front-end/src/app/shared/global-constants.ts
@@ -0,0 +1,16 @@
+export class GlobalConstants{
+  //message
+  public static genricError: string = "Something went wrong. Please try again later";
+
+  //Regex
+  public static nameRegex: string = "[a-zA-Z0-9 ]*";
+
+  public static emailRegex: string = "[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}";
+
+  public static contactNumberRegex: string = "^[e0-9]{8,8}$";
+
+ // public static passwordRegex : string = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d\w\W]{8,}$";
+
+  //Variable
+  public static error: string = "error";
+}
