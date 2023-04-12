4e5773fa7b7c395802eb1691a8e402872a24574b
Hassen-Rahali <hassen.rahali@esprit.tn>
2023-04-12 15:15:06 +0200

    create Change Password & Logout

--- front-end/src/app/layouts/full/header/header.component.ts
+++ front-end/src/app/layouts/full/header/header.component.ts
@@ -1,37 +1,36 @@
 import { Component } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { Router } from '@angular/router';
-import {ConfirmationComponent} from "../../../material-component/dialog/confirmation/confirmation.component";
-import {ChangePasswordComponent} from "../../../material-component/dialog/change-password/change-password.component";
+import { ChangePasswordComponent } from 'src/app/material-component/dialog/change-password/change-password.component';
+import { ConfirmationComponent } from 'src/app/material-component/dialog/confirmation/confirmation.component';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: []
+  styleUrls: [],
 })
 export class AppHeaderComponent {
   role: any;
-  constructor(private router: Router,
-    private dialog: MatDialog) {
+  constructor(private router: Router, private dialog: MatDialog) {}
 
-  }
-
-  logout(){
+  logout() {
     const dialogConfig = new MatDialogConfig();
-    dialogConfig.data ={
-      message: 'logout'
+    dialogConfig.data = {
+      message: 'Logout',
     };
-    const dialogRef = this.dialog.open(ConfirmationComponent,dialogConfig);
-    const sub = dialogRef.componentInstance.onEmitStatusChange.subscribe((user)=>{
-      dialogRef.close();
-      localStorage.clear();
-      this.router.navigate(['/']);
-    })
+    const dialogRef = this.dialog.open(ConfirmationComponent, dialogConfig);
+    const sub = dialogRef.componentInstance.onEmitStatusChange.subscribe(
+      (user: any) => {
+        dialogRef.close();
+        localStorage.clear();
+        this.router.navigate(['/']);
+      }
+    );
   }
 
-  changePassword(){
+  changePassword() {
     const dialogConfig = new MatDialogConfig();
-    dialogConfig.width= "550px";
-    this.dialog.open(ChangePasswordComponent,dialogConfig);
+    dialogConfig.width = '550px';
+    const dialogRef = this.dialog.open(ChangePasswordComponent, dialogConfig);
   }
 }
