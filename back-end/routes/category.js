09aee42e7cb976c415513f8ea265b4299c71522e
Hassen-Rahali <hassen.rahali@esprit.tn>
2023-04-12 15:01:46 +0200

    update category

--- back-end/routes/category.js
+++ back-end/routes/category.js
@@ -1,47 +1,46 @@
-const express = require('express');
-const connection = require('../connection');
+const express = require("express");
+const connection = require("../connection");
+const auth = require("../services/auth");
+const role = require("../services/checkRole");
+
 const router = express.Router();
-var auth= require('../services/authentication');
-var checkRole =require('../services/checkRole');
 
-router.post('/add',checkRole.checkRole,(req,res,next)=>{
-    let category = req.body;
-    query = "insert into category (name) values(?)";
-    connection.query(query,[category.name],(err,results)=>{
-        if(!err){
-            return res.status(200).json({message:"Category Added Successfuly"});
-        }
-        else{
-            return res.status(500).json(err);
-        }
-    })
-})
+router.post("/add", auth.authenticate, role.checkRole, (req, res, next) => {
+  let category = req.body;
+  let query = "insert into category (name) values(?)";
+  connection.query(query, [category.name], (err, results) => {
+    if (!err) {
+      return res.status(200).json({ message: "Category added successfully" });
+    } else {
+      return res.status(500).json({ err });
+    }
+  });
+});
 
-router.get('/get',(req,res,next)=>{
-     var query = "select *from category order by name";
-    connection.query(query,(err,results)=>{
-        if(!err){
-            return res.status(200).json(results);
-        }
-        else {
-            return res.status(500).json(err);
-        }
-    })
-})
+router.get("/get", (req, res, next) => {
+  let query = "select * from category order by name";
+  connection.query(query, (err, results) => {
+    if (!err) {
+      return res.status(200).json({ data: results });
+    } else {
+      return res.status(500).json({ err });
+    }
+  });
+});
 
-router.put('/update',checkRole.checkRole,(req,res,next)=>{
-    let product =req.body;
-    var query ="update category set name=? where id=?";
-    connection.query(query,[product.name,product.id],(err,results)=>{
-        if(!err){
-            if(results.affectedRows == 0){
-                return res.status(400).json({message:"Category id does not found"});
-            }
-            return res.status(200).json({message:"Category updated Successfuly"});
-        }
-        else {
-            return res.status(500).json(err);
-        }
-    }) 
-}) 
-module.exports = router ;
+router.patch("/update", auth.authenticate, role.checkRole, (req, res, next) => {
+  let product = req.body;
+  let query = "update category set name=? where id=?";
+  connection.query(query, [product.name, product.id], (err, results) => {
+    if (!err) {
+      if (results.affectedRows == 0) {
+        return res.status(404).json({ message: " Category ID not found" });
+      }
+      return res.status(200).json({ message: "Category updated successfully" });
+    } else {
+      return res.status(500).json({ err });
+    }
+  });
+});
+
+module.exports = router;
