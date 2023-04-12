94f5fd9b631261e51f94940a6d4574bd8909e65d
Hassen-Rahali <hassen.rahali@esprit.tn>
2023-04-12 15:18:15 +0200

    create login Page

--- front-end/src/app/login/login.component.ts
+++ front-end/src/app/login/login.component.ts
@@ -1,56 +1,66 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {Router} from "@angular/router";
-import {UserService} from "../services/user.service";
-import {SnackbarService} from "../services/snackbar.service";
-import {MatDialogRef} from "@angular/material/dialog";
-import {NgxUiLoaderService} from "ngx-ui-loader";
-import {GlobalConstants} from "../shared/global-constants";
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { SnackbarService } from '../services/snackbar.service';
+import { UserService } from '../services/user.service';
+import { GlobalConstants } from '../shared/global-constants';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-LoginForm:any = FormGroup;
-responseMessage:any;
-  constructor(private formBuilder:FormBuilder,
-              private router:Router,
-              private userService:UserService,
-              private snackbarService:SnackbarService,
-              private dialogRef:MatDialogRef<LoginComponent>,
-              private ngxService:NgxUiLoaderService) { }
+  loginForm: any = FormGroup;
+  responseMessage: any;
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private userService: UserService,
+    private snackBar: SnackbarService,
+    private dialogRef: MatDialogRef<LoginComponent>,
+    private ngxService: NgxUiLoaderService
+  ) {}
 
   ngOnInit(): void {
-    this.LoginForm = this.formBuilder.group({
-      email:[null,[Validators.required,Validators.pattern(GlobalConstants.emailRegex)]],
-      password:[null,Validators.required],
-    })
+    this.loginForm = this.fb.group({
+      email: [
+        null,
+        [Validators.required, Validators.pattern(GlobalConstants.emailRegex)],
+      ],
+      password: [null, [Validators.required, Validators.minLength(7)]],
+    });
   }
-  handleSubmit(){
+
+  handleSubmit() {
     this.ngxService.start();
-    var formData = this.LoginForm.value;
-    var data ={
+    let formData = this.loginForm.value;
+    var data = {
       email: formData.email,
       password: formData.password,
-    }
-    this.userService.login(data).subscribe((response:any)=> {
-      this.ngxService.stop();
-      this.dialogRef.close();
-      localStorage.setItem('token',response.token);
-      this.router.navigate(['/cafe/dashboard']);
-    },(error)=>{
-      this.ngxService.stop();
-      if(error.error?.message)
-      {
-        this.responseMessage = error.error?.message;
-      }
-      else {
-        this.responseMessage = GlobalConstants.genricError;
-      }
-      this.snackbarService.openSnackBar(this.responseMessage,GlobalConstants.error);
-    })
+    };
 
+    this.userService.login(data).subscribe(
+      (resp: any) => {
+        this.ngxService.stop();
+        this.dialogRef.close();
+        this.responseMessage = resp?.message;
+        localStorage.setItem('token', resp?.token)
+        this.snackBar.openSnackBar(this.responseMessage, '');
+        this.router.navigate(['/cafe/dashboard']);
+      },
+      (error) => {
+        this.ngxService.stop();
+        if (error.error?.message) {
+          this.responseMessage = error.error?.message;
+        } else {
+          this.responseMessage = GlobalConstants.genericError;
+        }
+        this.snackBar.openSnackBar(this.responseMessage, GlobalConstants.error);
+      }
+    );
   }
 }
