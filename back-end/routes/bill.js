84aaa3d0cda6d0911b69b69436cbfe8a0a047f27
Hassen-Rahali <hassen.rahali@esprit.tn>
2023-04-12 15:00:42 +0200

    create bill (pdf)

--- back-end/routes/bill.js
+++ back-end/routes/bill.js
@@ -1,101 +1,126 @@
-const express = require('express');
-const connection = require('../connection');
+const express = require("express");
+const connection = require("../connection");
+const auth = require("../services/auth");
+const ejs = require("ejs");
+const pdf = require("html-pdf");
+const path = require("path");
+const fs = require("fs");
+const uuid = require("uuid");
+
 const router = express.Router();
-let ejs = require('ejs');
-let pdf = require('html-pdf');
-let path = require('path');
-var fs = require('fs');
-var uuid = require('uuid');
-var auth= require('../services/authentication')
 
-router.post('/generateReport',auth.authenticateToken,(req,res)=>{
-    const generatedUuid = uuid.v1();
-    const orderDetails = req.body;
-    var productDetailsReport = JSON.parse(orderDetails.productDetails);
+router.post("/generateReport", auth.authenticate, (req, res) => {
+  const orderDetails = req.body;
+  const generatedUUID = uuid.v1();
+  let productDetailsReport = JSON.parse(orderDetails.productDetails);
+  let query =
 
-    query = "insert into bill (name,uuid,email,contactNumber,paymentMethod,total,productDetails,createdBy) values(?,?,?,?,?,?,?,?)";
-    connection.query(query,[orderDetails.name,generatedUuid,orderDetails.email,orderDetails.contactNumber,orderDetails.paymentMethod,orderDetails.totalAmount,orderDetails.productDetails,res.locals.email],(err,results)=>{
-        if(!err){
-            ejs.renderFile(path.join(__dirname,'',"report.ejs"),{productDetails:productDetailsReport,name:orderDetails.name,
-                email:orderDetails.email,contactNumber:orderDetails.contactNumber,paymentMethod:orderDetails.paymentMethod,
-                totalAmount:orderDetails.totalAmount},(err,results)=>{
-                if(err){
-                    return res.status(500).json(err);
+    "insert into bill (name, uuid, email, contactNumber, paymentMethod, total, productDetails, createdBy) values(?,?,?,?,?,?,?,?)";
+
+  connection.query(query, [
+      orderDetails.name,
+      generatedUUID,
+      orderDetails.email,
+      orderDetails.contactNumber,
+      orderDetails.paymentMethod,
+      orderDetails.totalAmount,
+      orderDetails.productDetails,
+      res.locals.email,
+    ],
+    (err, results) => {
+      if (!err) {
+        ejs.renderFile(
+          path.join(__dirname, "", "report.ejs"),
+          {
+            productDetails: productDetailsReport,
+            name: orderDetails.name,
+            email: orderDetails.email,
+            contactNumber: orderDetails.contactNumber,
+            paymentMethod: orderDetails.paymentMethod,
+            totalAmount: orderDetails.totalAmount,
+          },
+          (err, results) => {
+            if (err) {
+              return res.status(500).json({ err });
+            } else {
+              pdf.create(results).toFile("../generated_PDF/" + generatedUUID + ".pdf", (err, data) => {
+                    if (err) {
+                      console.log(err);
+                      return res.status(500).json({ err });
+                    } else {
+                      return res.status(200).json({ uuid: generatedUUID });
+                    }
+              });
+            }
+          });
+      } else {
+        return res.status(500).json({ err });
+      }
+    }
+  );
+});
+
+router.post("/getPDF", auth.authenticate, (req, res) => {
+  const orderDetails = req.body;
+  const pdfPath = "../generated_PDF/" + orderDetails.uuid + ".pdf";
+  if (fs.existsSync(pdfPath)) {
+    res.contentType("application/pdf");
+    fs.createReadStream(pdfPath).pipe(res);
+  } else {
+    let productDetailsReport = JSON.parse(orderDetails.productDetails);
+    ejs.renderFile(
+      path.join(__dirname, "", "report.ejs"),
+      {
+        productDetails: productDetailsReport,
+        name: orderDetails.name,
+        email: orderDetails.email,
+        contactNumber: orderDetails.contactNumber,
+        paymentMethod: orderDetails.paymentMethod,
+        totalAmount: orderDetails.totalAmount,
+      },
+      (err, results) => {
+        if (err) {
+          return res.status(200).json({ err });
+        } else {
+            pdf.create(results).toFile("./generated_PDF/" + orderDetails.uuid + ".pdf", (err, data) => {
+                if (err) {
+                  console.log(err);
+                  return res.status(500).json({ err });
                 }
                 else {
-                    pdf.create(results).toFile('./generated_pdf/'+generatedUuid+".pdf",function (err,data){
-                        if(err){
-                            console.log(err);
-                            return res.status(500).json(err);
-                        }
-                        else{
-                            return res.status(200).json({uuid: generatedUuid});
-                        }
-                    })
+                  res.contentType("application/pdf");
+                  fs.createReadStream(pdfPath).pipe(res);
                 }
-            })
+              });
         }
-        else{
-            return res.status(500).json(err);
-        }
-    })
-})
+      });
+  }
+});
 
-router.post('/getPdf',auth.authenticateToken,function (req,res){
-    const orderDetails = req.body;
-    const pdfPath = './generated_pdf/'+orderDetails.uuid+'.pdf';
-    if(fs.existsSync(pdfPath)){
-        res.contentType("application/pdf");
-        fs.createReadStream(pdfPath).pipe(res);
+router.get("/getBills", auth.authenticate, (req, res, next) => {
+  let query = "select * from bill order by id DESC";
+  connection.query(query, (err, results) => {
+    if (!err) {
+      return res.status(200).json({ data: results });
+    } else {
+      return res.status(500).json({ err });
     }
-    else{
-        var productDetailsReport = JSON.parse(orderDetails.productDetails);
-        ejs.renderFile(path.join(__dirname,'',"report.ejs"),{productDetails:productDetailsReport,name:orderDetails.name,
-            email:orderDetails.email,contactNumber:orderDetails.contactNumber,paymentMethod:orderDetails.paymentMethod,
-            totalAmount:orderDetails.totalAmount},(err,results)=>{
-            if(err){
-                return res.status(500).json(err);
-            }
-            else {
-                pdf.create(results).toFile('./generated_pdf/' + orderDetails.uuid + ".pdf",function (err,data){
-                    if(err){
-                        console.log(err);
-                        return res.status(500).json(err);
-                    }
-                    else{
-                        res.contentType("application/pdf");
-                        fs.createReadStream(pdfPath).pipe(res);
-                    }
-                })
-            }
-        })
-    }
-})
+  });
+});
 
-router.get('/getBills',auth.authenticateToken,(req,res,next)=>{
-    var query = "select * from bill order by id DESC";
-    connection.query(query,(err,results)=>{
-        if(!err){
-            return res.status(200).json(results);
-        }
-        else{
-            return res.status(500).json(err);
-        }
-    })
-})
-router.delete('/delete/:id',auth.authenticateToken, (req,res,next)=>{
-    const id = req.params.id;
-    var query = "delete from bill where id=?";
-    connection.query(query,[id],(err,results)=>{
-        if(!err){
-            if(results.affectedRows == 0){
-                return res.status(400).json({ message:"Bill id does not found"});
-            }
-            return res.status(200).json({ message:"Bill Deleted Successfully"});
-        }
-        else{
-            return res.status(500).json(err);
-        }
-    })
-})
+router.delete("/delete/:id", auth.authenticate, (req, res, next) => {
+  const id = req.params.id;
+  let query = "delete from bill where id=?";
+  connection.query(query, [id], (err, results) => {
+    if (!err) {
+      if (results.affectedRows == 0) {
+        return res.status(404).json({ message: "Bill ID not found" });
+      }
+      return res.status(200).json({ message: "Bill deleted successfully" });
+    } else {
+      return res.status(500).json({ err });
+    }
+  });
+});
+
 module.exports = router;
