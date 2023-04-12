50cde59c4efa2cd525c3a7c597f735344f48c9b2
Hassen-Rahali <hassen.rahali@esprit.tn>
2023-04-12 15:22:33 +0200

    create auth and token-interceptor

--- front-end/src/app/services/user.service.ts
+++ front-end/src/app/services/user.service.ts
@@ -1,38 +1,51 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {environment} from '../../environments/environment';
-import {HttpClient, HttpHeaders} from "@angular/common/http";
+import { environment } from '../../environments/environment';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-url = environment.apiUrl;
-  constructor(private httpClient:HttpClient) { }
+  url = environment.apiURL;
+  jsonHeader = {
+    headers: new HttpHeaders().set('Content-Type', 'application/json'),
+  };
 
-  signup(data:any){
-    return this.httpClient.post(this.url+"/user/signup/",data,{
-      headers : new HttpHeaders().set('content-Type',"application/json")
-    })
+  constructor(private http: HttpClient) {}
+
+  signup(data: any) {
+    return this.http.post(`${this.url}/user/signup`, data, this.jsonHeader);
   }
 
-  forgetPassword(data:any){
-    return this.httpClient.post(this.url+"/user/forgetPassword/",data,{
-      headers: new HttpHeaders().set('content-Type',"application/json")
-    })
+  forgotPassword(data: any) {
+    return this.http.post(
+      `${this.url}/user/forgotPassword`,
+      data,
+      this.jsonHeader
+    );
   }
 
-  login(data:any){
-    return this.httpClient.post(this.url+"/user/login/",data,{
-      headers: new HttpHeaders().set('content-Type',"application/json")
-    })
+  login(data: any) {
+    return this.http.post(`${this.url}/user/login`, data, this.jsonHeader);
   }
-checkToken(){
-    return this.httpClient.get(this.url+"/user/checkToken");
-}
 
-changePassword(data:any){
-  return this.httpClient.post(this.url+"/user/changePassword",data,{
-    headers: new HttpHeaders().set('content-Type',"application/json")
-  })
-}
+  checkToken() {
+    return this.http.get(`${this.url}/user/checkToken`);
+  }
 
+  changePassword(data: any) {
+    return this.http.post(
+      `${this.url}/user/changePassword`,
+      data,
+      this.jsonHeader
+    );
+  }
+
+  getUsers() {
+    return this.http.get(`${this.url}/user/get`);
+  }
+
+  update(data: any) {
+    return this.http.patch(`${this.url}/user/update`, data, this.jsonHeader);
+  }
 }
