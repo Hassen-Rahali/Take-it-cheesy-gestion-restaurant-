fbb676780e0badd43e50cd3c9a6c6ea1b5d9d83b
Hassen-Rahali <hassen.rahali@esprit.tn>
2023-04-12 15:01:22 +0200

    update dashboard auth

--- back-end/routes/dashboard.js
+++ back-end/routes/dashboard.js
@@ -1,63 +1,47 @@
-// Import the Express.js framework
-const express = require('express');
-// Import the database connection module
-const connection = require('../connection');
-// Create a router instance
+const express = require("express");
+const connection = require("../connection");
+const auth = require("../services/auth");
+const role = require("../services/checkRole");
+
 const router = express.Router();
-// Import the authentication middleware
-var auth = require('../services/authentication');
 
-// Define a GET route for getting various counts
-router.get('/details', (req, res, next) => {
-// Declare variables to store the counts
-    var categoryCount;
-    var productCount;
-    var billCount;
-// Query the database to get the count of categories
-    var categoryQuery = "select count(id) as categoryCount from category";
-    connection.query(categoryQuery, (err, results) => {
-        // If no error, store the category count
-        if (!err) {
-            categoryCount = results[0].categoryCount;
-        }
-        // If there's an error, return a 500 status and the error message
-        else {
-            return res.status(500).json(err);
-        }
-    })
-// Query the database to get the count of products
-    var productQuery = "select count(id) as productCount from product";
-    connection.query(productQuery, (err, results) => {
-        // If no error, store the product count
-        if (!err) {
-            productCount = results[0].productCount;
-        }
-        // If there's an error, return a 500 status and the error message
-        else {
-            return res.status(500).json(err);
-        }
-    })
-// Query the database to get the count of bills
-    var billQuery = "select count(id) as billCount from bill";
-    connection.query(billQuery, (err, results) => {
-        // If no error, store the bill count
-        if (!err) {
-            billCount = results[0].billCount;
-            // Create a data object with the counts
-            var data = {
-                category: categoryCount,
-                product: productCount,
-                bill: billCount,
-            };
-            // Return a 200 status and the data object as JSON
-            return res.status(200).json(data);
-        }
-        // If there's an error, return a 500 status and the error message
-        else {
-            return res.status(500).json(err);
-        }
-    })
-})
+router.get("/details", auth.authenticate, (req, res, next) => {
+  let categoryCount;
+  let productCount;
+  let billCount;
 
-// Export the router module
+  let queryCategory = "select count(id) as categoryCount from category";
+  connection.query(queryCategory, (err, results) => {
+    if (!err) {
+      categoryCount = results[0].categoryCount;
+    } else {
+      return res.status(500).json({ err });
+    }
+  });
+
+  let queryProduct = "select count(id) as productCount from product";
+  connection.query(queryProduct, (err, results) => {
+    if (!err) {
+      productCount = results[0].productCount;
+    } else {
+      return res.status(500).json({ err });
+    }
+  });
+
+  let queryBill = "select count(id) as billCount from bill";
+  connection.query(queryBill, (err, results) => {
+    if (!err) {
+      billCount = results[0].billCount;
+      let data = {
+        category: categoryCount,
+        product: productCount,
+        bill: billCount,
+      };
+      return res.status(200).json({ data });
+    } else {
+      return res.status(500).json({ err });
+    }
+  });
+});
+
 module.exports = router;
